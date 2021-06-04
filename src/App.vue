55bdbe03d5096e9272bcaba64621d0ad19f74a45
Kejin <kdon0003@student.monash.edu>
2021-06-04 17:39:11 +0800

    header / footer开发

--- src/App.vue
+++ src/App.vue
@@ -9,41 +9,33 @@
 </template>
 
 <script>
-import jsonp from 'jsonp'
+// import jsonp from 'jsonp'
 export default {
   data() {
     return {
-      data: ''
+      data: '',
+      res: {}
     }
   },
   mounted(){
-    let url = "/activity/servicetime"
-    jsonp(url, (err, res) => {
-      let result = res
-      this.data = result
-    })
+    // let url = "/activity/servicetime"
+    // jsonp(url, (err, res) => {
+    //   let result = res
+    //   this.data = result
+    // })
+    // 本地加载请求静态json文件的形式
+    // this.axios.get('/mock/user/login.json').then((res) => {
+    //   this.res = res
+    // })
+
+    // 通过easy-mock平台实现数据mock
+    // 本地继承mockjs实现数据mock
+    // this.axios.get('/user/login').then((res) => {
+    //   this.res = res
+    // })
   }
 }
 </script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+@import './assets/scss/reset.scss'
 </style>
